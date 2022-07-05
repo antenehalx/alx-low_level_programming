fb1a9c4e54b91a983d13ca306402cdf6a692a2cd
alxse <alxse@github.com>
2022-07-05 15:02:35 -0700

    Hot fix

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,30 +1,20 @@
-#include "main.h"
+#include <unistd.h>
+
+#include "holberton.h"
 
 
 
 /**
- * main - Prints Holberton as a message.
- * Return: Always 0 (Success)
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 
-int main(void)
+int _putchar(char c)
 
 {
 
-    int str[] = {72, 111, 108, 98, 101, 114, 116, 111, 110};
+		return (write(1, &c, 1));
 
-			int count, sz;
-
-
-
-	       sz = sizeof(str) / sizeof(int);
-
-	     for (count = 0; count < sz; count++)
-
-   	    {
-				_putchar(str[count]);
-
-											}
-
-		_putchar('\n');
-		  return (0);
+}
