1b44fc591048edddb57c08678f2bec1ec5a0f9b1
alxse <alxse@github.com>
2022-07-06 15:28:11 -0700

    Hot fix

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,8 +6,7 @@
  */
 
 int main(void)
-	
-{
+	{
 
 	char abe[] = "_putchar";
 
@@ -18,15 +17,10 @@ int main(void)
 
 
 	for (c = 0; c < 8; c++)
-
-	{
-	
-			_putchar(abe[c]);
-	
-		}
-
-	_putchar('\n');
+            {	
+      _putchar(abe[c]);
+	     }
+      _putchar('\n');
 
 	return (0);
-
 }
