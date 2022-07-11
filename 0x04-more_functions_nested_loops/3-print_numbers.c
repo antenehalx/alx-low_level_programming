1f7c5096f44a008f208a6b6474defefbd736d3a5
alxse <alxse@github.com>
2022-07-11 16:13:04 -0700

    fix

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -9,4 +9,21 @@ int main(void)
 {
     print_numbers();
     return (0);
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - Print numbers between 0 to 9 incl.
+ *
+ * Return: Void.
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i + '0');
+	}
+	_putchar('\n');
 }
