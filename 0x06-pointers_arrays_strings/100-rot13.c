e4f20df585c267148ae61e62d47275282ecc08bd
alxse <alxse@github.com>
2022-07-13 13:41:26 -0700

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,41 +1,27 @@
-#include <stddef.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include "main.h"
 
-/*
-ROT13 is a simple letter substitution cipher that replaces a letter with the letter 13 letters after it in the alphabet.
-ROT13 is an example of the Caesar cipher.
-This function takes a string and returns the string ciphered with Rot13. 
-If there are numbers or special characters included in the string, they are returned as they are. 
-Only letters from the latin/english alphabet are shifted, like in the original Rot13 "implementation".
-*/
-
-char *rot13(const char *src)
+/**
+ * rot13 -  a   function that encodes a string using rot13.
+ * @s: An input string to encode using rot13
+ * Return: An encode string
+ */
+char *rot13(char *s)
 {
-    if(src == NULL){
-      return NULL;
-    }
-  
-    char* result = malloc(strlen(src));
-    
-    if(result != NULL){
-      strcpy(result, src);
-      char* current_char = result;
-      
-      while(*current_char != '\0'){
-        //Only increment alphabet characters
-        if((*current_char >= 97 && *current_char <= 122) || (*current_char >= 65 && *current_char <= 90)){
-          if(*current_char > 109 || (*current_char > 77 && *current_char < 91)){
-            //Characters that wrap around to the start of the alphabet
-            *current_char -= 13;
-          }else{
-            //Characters that can be safely incremented
-            *current_char += 13;
-          }
-        }
-        current_char++;
-      }
-    }
-    return result;
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		while ((s[i] >= 'a' && s[i] <= 'z') ||
+				(s[i] >= 'A' && s[i] <= 'Z'))
+		{
+			if ((s[i] >= 'a' && s[i] <= 'm') ||
+					(s[i] >= 'A' && s[i] <= 'M'))
+				s[i] += 13;
+			else
+				s[i] -= 13;
+			i++;
+		}
+		i++;
+	}
+	return (s);
 }
